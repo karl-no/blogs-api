f9d26aebb41ee3e5dd66596d66b79529ccece6d2
karl-no <karl.publicidade@gmail.com>
2022-12-01 17:59:30 -0300

    Requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users',
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: 'userId', as: 'blogPosts'
+    });
+  }
+
+  return User;
+}
