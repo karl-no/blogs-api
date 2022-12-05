8f5b1a0e4ebdcd1e945bd3f6d2fd82d7465a576c
karl-no <karl.publicidade@gmail.com>
2022-12-05 17:59:06 -0300

    Se o avaliador do GitHub deixar, eu passo a 8 e a 9

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { loginRouter, userRouter, categoryRouter } = require('./routers');
 const app = express();
 
 app.use(express.json());
+
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
 app.use('/categories', categoryRouter);
